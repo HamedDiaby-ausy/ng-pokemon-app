9b09fbec1534b4503714b7e8a9c15dd26240fa63
Hamed Diaby KOUMBA <hameddiaby.koumba@randstaddigital.fr>
2024-12-10 16:36:04 +0100

    creer une custom methode: selectPokemon

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,6 +8,11 @@ export class AppComponent implements OnInit {
   pokemonList = ['Bulbizarre', 'Salamèche', 'Carapuce'];
 
   ngOnInit(): void {
-    console.table(this.pokemonList)
+    console.table(this.pokemonList);
+    this.selectPokemon(this.pokemonList[0]);
+  }
+
+  selectPokemon(pokemonName: string){
+    console.log(`Vous avez sélèctionnez le pokemon ${pokemonName}`);
   }
 }
