7e57b41d4a1ef10b97b2655970d2d0e2b23e337d
Hamed Diaby KOUMBA <hameddiaby.koumba@randstaddigital.fr>
2024-12-11 13:25:52 +0100

    Exercice 1: ma solution

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,18 +1,19 @@
 import { Component, OnInit } from '@angular/core';
+import { POKEMONS } from './mock-pokemon-list';
+import { Pokemon } from './pokemon';
 
 @Component({
   selector: 'app-root',
-  template: `<h1>Welcome to {{pokemonList[0]}}!</h1>`
+  template: `<h1>Liste des pokémons</h1>`
 })
 export class AppComponent implements OnInit {
-  pokemonList = ['Bulbizarre', 'Salamèche', 'Carapuce'];
 
   ngOnInit(): void {
-    console.table(this.pokemonList);
-    this.selectPokemon(this.pokemonList[0]);
+    console.table(POKEMONS);
+    this.selectPokemon(POKEMONS[0]);
   }
 
-  selectPokemon(pokemonName: string){
-    console.log(`Vous avez sélèctionnez le pokemon ${pokemonName}`);
+  selectPokemon(pokemon: Pokemon){
+    console.log(`Vous avez sélèctionnez le pokemon ${pokemon.name}`);
   }
 }
